bb94534e2e6b3e326de48b7d385fe4041c782cb4
Pranshu321 <pranshujain0111@gmail.com>
2022-09-18 23:43:29 +0530

    link added

--- frontend/src/components/chatBot/components/Navbar.jsx
+++ frontend/src/components/chatBot/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -18,8 +19,10 @@ const Navbar = () => {
             <div class="usa-logo margin-0" id="basic-mega-logo">
               <em class="usa-logo__text">
                 <div className="display-flex flex-row align-items-center">
+                <Link to={"/"}>
                 <img width={70} height={70} src={"https://user-images.githubusercontent.com/86917304/189974862-abcfeac0-b5ed-44d2-8eb4-8e2a05a59294.png"} alt="logo" />
                   <h3 className="display-inline-block padding-left-1" style={{color: "white"}}>Wecare Checker</h3>
+                </Link>
                 </div>
               </em>
             </div>
