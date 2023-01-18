7c3794d47d86c891c0bae24fed7842127733829b
Sreetama <fool100abc@gmail.com>
2023-01-18 21:51:13 +0530

    Added Fun  bot changes

--- frontend/src/ChatbotAPI.js
+++ frontend/src/ChatbotAPI.js
@@ -3,8 +3,6 @@ import axios from "axios";
 async function moodGuesser(message) {
 	// http://tweetmoodchk.d7cgcshfajbxf6dv.eastus.azurecontainer.io/mood
 
-	
-
 	const response = axios
 		.get("https://official-joke-api.appspot.com/random_joke")
 		.then(function (response) {
@@ -40,32 +38,72 @@ async function getMeme() {
 	return response.data.setup + response.data.punchline;
 }
 
+// const API = {
+// 	GetChatbotResponse: async (message) => {
+// 		return new Promise(function (resolve, reject) {
+// 			setTimeout(function () {
+// 				if (message === "hi" || message === "hello" || message === "hey")
+// 					resolve(
+// 						" Hi ! How was ur Day :) ? "
+// 					);
+// 				else if (message === "joke") {
+// 					axios
+// 						.get("https://official-joke-api.appspot.com/random_joke")
+// 						.then(function (response) {
+// 							// handle success
+// 							console.log(response.data);
+// 							resolve(response.data.setup + " " + response.data.punchline);
+// 						})
+// 						.catch(function (error) {
+// 							// handle error
+// 							console.log(error);
+// 						});
+// 				} else {
+// 					// resolve("I am  your mood guesser bot 😀, I will send you jokes if you are sad ? I can your mood for sure ;)");
+					
+				
+// 				}
+// 			}, 1000);
+// 		});
+// 	},
+// };
+
 const API = {
 	GetChatbotResponse: async (message) => {
-		return new Promise(function (resolve, reject) {
-			setTimeout(function () {
-				if (message === "hi" || message === "hello" || message === "hey")
-					resolve(
-						"Here I am  your mood guesser bot , I will send you jokes which will make you happy! Plese type 'joke'. "
-					);
-				else if (message === "joke") {
-					axios
-						.get("https://official-joke-api.appspot.com/random_joke")
-						.then(function (response) {
-							// handle success
-							console.log(response.data);
-							resolve(response.data.setup + " " + response.data.punchline);
-						})
-						.catch(function (error) {
-							// handle error
-							console.log(error);
-						});
-				} else {
-					resolve("Here I am  your mood guesser bot , I will send you jokes which will make you happy! Plese type 'joke'. ");
-				}
-			}, 1000);
-		});
+	  return new Promise(async (resolve, reject) => {
+		try {
+		//   resolve("I am  your mood guesser bot 😀, I will send you jokes if you are sad ;)");
+		  if (message === "hi" || message === "hello" || message === "hey") {
+			resolve(" Hi 👋 ! How was ur Day :) ? ");
+		  }
+		  else if (message === "joke") {
+			// Call the joke API
+			const jokeResponse = await axios.get('https://official-joke-api.appspot.com/random_joke');
+			const { setup, punchline } = jokeResponse.data;
+			resolve(`${setup} ${punchline}`);
+		  }
+		  else {
+			// Call the mood detection API
+			const moodResponse = await axios.post('http://127.0.0.1:8000/mood',{
+                "mood": message
+            });
+			const {flag, reply } = moodResponse.data;
+  
+			if (flag) {
+			  // Call the joke API
+			  const jokeResponse = await axios.get('https://official-joke-api.appspot.com/random_joke');
+			  const { setup, punchline } = jokeResponse.data;
+			  resolve(`${reply} ${setup} ${punchline}`);
+			} else {
+			  resolve(`${reply} Have a good day!`);
+			}
+		  }
+		} catch (error) {
+		  console.log(error);
+		  reject(error);
+		}
+	  });
 	},
-};
-
+  };
+  
 export default API;
